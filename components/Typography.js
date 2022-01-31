28aebb702f519ffed9df3d7884d1b3df30e909ff
giang <nguyengiang.nchg@gmail.com>
2022-01-31 15:26:25 +0200

    add typography and button component

---
+++ components/Typography.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Text } from 'react-native';
+
+import { theme } from '../themes';
+
+const Typography = ({ variant, color, fontWeight, text }) => {
+  const getTextFontSize = (variant) => {
+    let fontSize = 12;
+    switch (variant) {
+      case 'h1':
+        fontSize = 48;
+        break;
+      case 'h2':
+        fontSize = 24;
+        break;
+      case 'h3':
+        fontSize = 18;
+        break;
+      case 'h4':
+        fontSize = 16;
+        break;
+      case 'h5':
+        fontSize = 14;
+        break;
+      default:
+        return;
+    }
+    return fontSize;
+  };
+
+  return (
+    <Text
+      style={{
+        fontFamily: 'Avenir',
+        fontSize: getTextFontSize(variant),
+        color: color ?? theme.colors.white,
+        fontWeight: fontWeight ?? '400',
+      }}
+    >
+      {text}
+    </Text>
+  );
+};
+
+Typography.propTypes = {
+  variant: PropTypes.string.isRequired,
+  text: PropTypes.string.isRequired,
+  color: PropTypes.string,
+  fontWeight: PropTypes.string,
+};
+
+export default Typography;
