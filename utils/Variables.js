18ff99e472117ce3fc4ea4c58483ceda415ac55a
GiangNguyen <31731738+GiangNguyen1207@users.noreply.github.com>
2022-02-15 21:23:25 +0200

    Delete Variables.js

--- utils/Variables.js
+++
@@ -1,5 +0,0 @@
-const uploadsUrl = 'https://media.mw.metropolia.fi/wbma/uploads/';
-const apiUrl = 'https://media.mw.metropolia.fi/wbma/';
-const appID = 'KRITICS_TEMP_ID';
-
-export { uploadsUrl, apiUrl, appID };
