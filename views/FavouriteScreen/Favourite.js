5eb5c0344da065e071a8571e693e212a72fa51cf
Miro <mirota@metropolia.fi>
2022-03-08 16:57:01 +0200

    favourite collapsible header

--- views/FavouriteScreen/Favourite.js
+++ views/FavouriteScreen/Favourite.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { FlatList, View } from 'react-native';
+import { FlatList, View, Animated, Image, StyleSheet } from 'react-native';
 import { useIsFocused } from '@react-navigation/native';
 import PropTypes from 'prop-types';
 import LottieView from 'lottie-react-native';
@@ -9,6 +9,8 @@ import { theme } from '../../themes';
 import { useCommentRating } from '../../hooks/useCommentRating';
 import { useMedia } from '../../hooks/useMedia';
 import { useFavourite } from '../../hooks/useFavourite';
+import { useCollapsibleHeader } from 'react-navigation-collapsible';
+import ScreenLayout from '../../components/ScreenLayout';
 
 const Favourite = ({ navigation }) => {
   const { getRating, getAverageRating } = useCommentRating();
@@ -18,6 +20,26 @@ const Favourite = ({ navigation }) => {
   const [refresh, setRefresh] = useState(false);
   const [renderedFavouriteList, setRenderedFavouriteList] = useState([]);
   const [screenLoading, setScreenLoading] = useState(false);
+  const { onScroll, containerPaddingTop, scrollIndicatorInsetTop } =
+    useCollapsibleHeader({
+      navigationOptions: {
+        headerStyle: {
+          height: 250,
+        },
+        headerTitleStyle: {
+          color: '#fff',
+        },
+        headerShown: true,
+        headerBackground: (
+          <Image
+            source={require('../../assets/batman.jpeg')}
+            style={styles.overlay}
+          />
+        ),
+        headerTitle: 'Favourites',
+      },
+      config: { collapsedColor: theme.colors.appBackground },
+    });
 
   useEffect(() => {
     async function getFavouriteAndRating() {
@@ -49,8 +71,9 @@ const Favourite = ({ navigation }) => {
     getFavouriteAndRating();
   }, [mediaArray, favouriteList]);
 
+  console.log();
   return (
-    <View style={{ flex: 1 }}>
+    <ScreenLayout>
       {screenLoading ? (
         <LottieView
           source={require('../../assets/lottie/loading.json')}
@@ -58,28 +81,49 @@ const Favourite = ({ navigation }) => {
           loop
         />
       ) : (
-        <FlatList
-          numColumns={3}
-          extraData={refresh}
-          data={renderedFavouriteList}
-          keyExtractor={(item) => item.file_id.toString()}
-          renderItem={({ item }) => (
-            <MovieCard
-              rating={4}
-              item={item}
-              cardStyle={{ marginVertical: theme.spacings.xxs }}
-              navigation={navigation}
-              showTagIcon={false}
-            />
-          )}
-        />
+        <View>
+          <Animated.FlatList
+            numColumns={3}
+            onScroll={onScroll}
+            extraData={refresh}
+            data={renderedFavouriteList}
+            contentContainerStyle={{
+              paddingTop: favouriteList.length > 0 ? containerPaddingTop : 0,
+            }}
+            scrollIndicatorInsets={{ top: scrollIndicatorInsetTop }}
+            keyExtractor={(item) => item.file_id.toString()}
+            renderItem={({ item }) => (
+              <MovieCard
+                rating={4}
+                item={item}
+                cardStyle={{ marginVertical: theme.spacings.xxs }}
+                navigation={navigation}
+                showTagIcon={false}
+              />
+            )}
+          />
+        </View>
       )}
-    </View>
+    </ScreenLayout>
   );
 };
 
 Favourite.propTypes = {
   navigation: PropTypes.object,
 };
+
+const styles = StyleSheet.create({
+  overlay: {
+    width: '100%',
+    position: 'absolute',
+    left: 0,
+    top: 0,
+    bottom: 0,
+    right: 0,
+    opacity: 0.7,
+    backgroundColor: theme.colors.black,
+    height: 250,
+  },
+});
 
 export default Favourite;
